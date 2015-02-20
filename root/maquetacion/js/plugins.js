73ae41e9ca2dbc22a336378c51bc755412ce9beb
Saúl Solórzano <hola@saulsolorzano.com>
2015-02-20 13:00:49 -0300

    Actualizando el plugin de mismo alto

--- root/maquetacion/js/plugins.js
+++ root/maquetacion/js/plugins.js
@@ -1,16 +1,57 @@
 (function ( $ ) {
-	$.fn.mantenerAlto = function(selector){
-		return this.each(function(){
-			var maxHeight	= -1,
-				$this		= $(this),
-				elemento	= $this.find(selector);
+	"use strict";
+	$.fn.mismoAlto   = function( options ) {
 
-			elemento.each(function() {
-				maxHeight = maxHeight > $(this).height() ? maxHeight : $(this).height();
-			});
-			elemento.each(function() {
-				$(this).height(maxHeight);
-			});
+		// Opciones del plugin. Primero el selector
+		// Luego un callback
+		var settings    = $.extend({
+			item:       null,
+			complete:   null
+		}, options);
+
+		// Variables generales del código
+		var currentTallest = 0,
+			currentRowStart = 0,
+			rowDivs = new Array(),
+			$el,
+			topPosition = 0;
+
+		return this.each( function(){
+			var $el			=	$(this),
+				currentDiv,
+				topPostion	=	$el.position().top;
+
+
+			if ( currentRowStart != topPostion ) {
+				// Tenemos una nueva fila, colocamos todos los altos
+				for ( currentDiv = 0 ; currentDiv < rowDivs.length ; currentDiv++ ) {
+					rowDivs[currentDiv].height(currentTallest);
+				}
+
+				// Colocamos las variables para la nueva fila
+				rowDivs.length	= 0; // Vaciamos el array
+				currentRowStart	= topPostion;
+				currentTallest	= $el.outerHeight();
+				rowDivs.push($el);
+
+			} else {
+
+				// Nos encontramos con otro div en la misma fila
+				// Lo agregamos a la lista si es más alto
+				rowDivs.push( $el );
+				currentTallest = ( currentTallest < $el.height() ) ? ( $el.height() ) : (currentTallest);
+			}
+
+			// Última fija
+			for ( currentDiv = 0 ; currentDiv < rowDivs.length ; currentDiv++ ) {
+				rowDivs[currentDiv].outerHeight(currentTallest);
+			}
+
+			// Callback una vez la función esté lista
+			if ( $.isFunction( settings.complete ) ) {
+				settings.complete.call( this );
+			}
 		});
 	};
+
 }( jQuery ));
