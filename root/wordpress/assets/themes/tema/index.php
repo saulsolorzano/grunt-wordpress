f4f6ca6c072b5e3f5d182abfc35850896f2fcd93
Saúl Solórzano <hola@saulsolorzano.com>
2016-04-11 11:44:33 -0300

    Acomodando error

--- root/wordpress/assets/themes/tema/index.php
+++ root/wordpress/assets/themes/tema/index.php
@@ -8,7 +8,6 @@
  * @author		{%= author_name %} <{%= author_email %}>
  * @version		{%= version %}
  */
- */
 if (!class_exists('Timber')) {
     echo 'Timber not activated. Make sure you activate the plugin.';
     return;
