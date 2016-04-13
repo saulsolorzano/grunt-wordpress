4924ccefad568eabda01e7f89af140dbc3f48d6e
Saúl Solórzano <hola@saulsolorzano.com>
2016-04-13 12:45:10 -0300

    Acomodando función de correo

--- root/maquetacion/js/plugins/validate.js
+++ root/maquetacion/js/plugins/validate.js
@@ -1,7 +1,8 @@
 (function ( $ ) {
     $.validator.addMethod("customemail", 
         function(value, element) {
-            return /^([a-zA-Z0-9_.-+])+\@(([a-zA-Z0-9-])+.)+([a-zA-Z0-9]{2,4})+$/.test(value);
+            var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+            return regex.test(value);
         }, 
         "Tu correo no es válido"
     );
