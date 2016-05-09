ea6906acfff5cac4b70e6785b7fd9cd986394c17
Saúl Solórzano <hola@saulsolorzano.com>
2016-05-09 09:18:07 -0300

    Agregando función de jquery

--- root/wordpress/assets/themes/tema/lib/theme-functions.php
+++ root/wordpress/assets/themes/tema/lib/theme-functions.php
@@ -18,11 +18,20 @@ function mb_remove_script_version( $src ){
  * Registrando JS para front de la página - Footer
  */
 function ss_scripts() {
-	wp_register_script( 'main-script', get_template_directory_uri() . '/js/app.min.js', array( 'jquery'), '1.0.0', true );
-	wp_register_script( 'modernizr-script', get_template_directory_uri() . '/js/vendor/modernizr.js', array(), '2.8.3', false );
-	wp_enqueue_script( 'main-script' );
-	wp_enqueue_script( 'modernizr-script' );
+    if ( ! is_admin() ) {
+        wp_deregister_script('jquery');
+        wp_register_script('jquery', (get_template_directory_uri() . "/js/vendor/jquery-1.12.1.min.js"), false, '1.12.1');
+        wp_enqueue_script('jquery');
+        wp_deregister_script('jquery-migrate');
+        wp_register_script('jquery-migrate', (get_template_directory_uri() . "/js/vendor/jquery-migrate-1.2.1.min.js"), false, '1.2.1  ');
+        wp_enqueue_script('jquery-migrate');
+    }
+    wp_register_script('main-script', get_template_directory_uri() . '/js/app.min.js', array('jquery'), '1.0.0', true);
+    wp_register_script('modernizr-script', get_template_directory_uri() . '/js/vendor/modernizr.js', array(), '2.8.3', false);
+    wp_enqueue_script('main-script');
+    wp_enqueue_script('modernizr-script');
 }
+
 /**
  * Excerpt más elegante
  */
