c504830f6c298941dba78baa83f7d7fc9172698d
Saúl Solórzano <hola@saulsolorzano.com>
2017-01-23 13:03:23 -0300

    Nueva clase para los correos, cortesia de @ciul

--- root/wordpress/assets/themes/tema/functions.php
+++ root/wordpress/assets/themes/tema/functions.php
@@ -13,6 +13,7 @@
 require_once( get_template_directory() . '/lib/init.php');
 require_once( get_template_directory() . '/lib/theme-functions.php');
 require_once( get_template_directory() . '/lib/theme-helpers.php');
+require_once( get_template_directory() . '/lib/theme-mails.php');
 
 
 /* * **************************************
