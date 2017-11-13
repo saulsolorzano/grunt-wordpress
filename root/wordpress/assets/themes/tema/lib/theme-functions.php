ba4ce2a9750c2a88c5fcde6ddba1ad94b5756a14
Juan Pablo Pardo Malfanti <pardomalfanti@gmail.com>
2017-11-13 13:11:51 -0300

    opciones del sitio

--- root/wordpress/assets/themes/tema/lib/theme-functions.php
+++ root/wordpress/assets/themes/tema/lib/theme-functions.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Iniciando funciones del tema
  *
@@ -10,46 +11,46 @@
 /**
  * Remove Query Strings From Static Resources
  */
-function mb_remove_script_version( $src ){
-	$parts = explode( '?', $src );
-	return $parts[0];
+function mb_remove_script_version($src) {
+    $parts = explode('?', $src);
+    return $parts[0];
 }
+
 /**
  * Registrando JS para front de la página - Footer
  */
 function ss_scripts() {
-    if ( ! is_admin() ) {
+    if (!is_admin()) {
         wp_deregister_script('jquery');
         wp_deregister_script('jquery-migrate');
     }
     $main_js = filemtime(get_stylesheet_directory() . '/js/app.min.js');
-    wp_register_script('main-script', get_template_directory_uri() . '/js/app.min.js', array(), '1.'.date ("Ymd", $main_js).'.'.date ("His", $main_js), true);
+    wp_register_script('main-script', get_template_directory_uri() . '/js/app.min.js', array(), '1.' . date("Ymd", $main_js) . '.' . date("His", $main_js), true);
     wp_enqueue_script('main-script');
 }
-
 
 /**
  * Excerpt más elegante
  */
 function new_excerpt_more($excerpt) {
-	return str_replace('[...]', ' ', $excerpt);
+    return str_replace('[...]', ' ', $excerpt);
 }
+
 /**
  * Quitando Width y Height de todas las imagenes - Sin esto el responsive no sirve
  */
-function remove_width_attribute( $html ) {
-	 $html = preg_replace( '/(width|height)="\d*"\s/', "", $html );
-	 return $html;
+function remove_width_attribute($html) {
+    $html = preg_replace('/(width|height)="\d*"\s/', "", $html);
+    return $html;
 }
-
 
 /**
  * Agregar soporte para subir un svg
  */
 function custom_upload_mimes($existing_mimes = array()) {
-		$existing_mimes['svg'] = 'image/svg+xml';
-		$existing_mimes['svgz'] = 'image/svg+xml';
-		return $existing_mimes;
+    $existing_mimes['svg'] = 'image/svg+xml';
+    $existing_mimes['svgz'] = 'image/svg+xml';
+    return $existing_mimes;
 }
 
 add_filter('upload_mimes', 'custom_upload_mimes');
@@ -59,64 +60,75 @@ add_filter('upload_mimes', 'custom_upload_mimes');
  */
 if (function_exists('acf_add_options_page')) {
 
-		acf_add_options_page(array(
-				'page_title' => 'Opciones del Tema',
-				'menu_title' => 'Opciones',
-				'menu_slug' => 'opciones-generales-tema',
-				'icon_url' => 'dashicons-hammer',
-				'capability' => 'edit_posts',
-				'redirect' => false
-		));
+    acf_add_options_page(array(
+        'page_title' => 'Opciones del Tema',
+        'menu_title' => 'Opciones del Tema',
+        'menu_slug' => 'opciones-generales-tema',
+        'icon_url' => 'dashicons-hammer',
+        'capability' => 'edit_posts',
+        'redirect' => false
+    ));
+
+    acf_add_options_sub_page(array(
+        'page_title' => 'Opciones de correos',
+        'menu_title' => 'Opciones de correos',
+        'parent_slug' => 'opciones-generales-tema',
+    ));
 }
 /*
  * Ocultar barra de abministración
  *  */
 add_filter('show_admin_bar', '__return_false');
+
 /**
  * Limpiando nombre de archivo antes de la subida
  * @param  string $filename pasamos el nombre del archivo
  * @return string           nos retorna el nombre del archivo sin caracteres especiales
  */
 function sanitize_filename_on_upload($filename) {
-	$exp = explode('.',$filename);
-	$ext = end($exp);
-	// Replace all weird characters
-	$sanitized = preg_replace('/[^a-zA-Z0-9-_.]/','', substr($filename, 0, -(strlen($ext)+1)));
-	// Replace dots inside filename
-	$sanitized = str_replace('.','-', $sanitized);
-	return strtolower($sanitized.'.'.$ext);
+    $exp = explode('.', $filename);
+    $ext = end($exp);
+    // Replace all weird characters
+    $sanitized = preg_replace('/[^a-zA-Z0-9-_.]/', '', substr($filename, 0, -(strlen($ext) + 1)));
+    // Replace dots inside filename
+    $sanitized = str_replace('.', '-', $sanitized);
+    return strtolower($sanitized . '.' . $ext);
 }
 
 add_filter('sanitize_file_name', 'sanitize_filename_on_upload', 10);
+
 function css_acf() {
     echo '<style>.acf-field .acf-label {margin: 0 0 25px;}</style>';
 }
+
 /**
  * Colocando Créditos en footer de Wordpress
  */
-function modify_footer_admin () {
-	echo 'Creado por <a href="{%= author_url %}">{%= author_name %}</a>. Potenciado por <a href="http://www.wordpress.org">WordPress</a>';
+function modify_footer_admin() {
+    echo 'Creado por <a href="{%= author_url %}">{%= author_name %}</a>. Potenciado por <a href="http://www.wordpress.org">WordPress</a>';
 }
+
 /**
  * Colocando logo de Saul en pantalla de login
  */
 function login_styles() {
-	echo '<style type="text/css">body.login #login h1 a { background: url('. get_bloginfo('template_directory') .'/img/wdt_logo.png) no-repeat center top; height:146px; width:326px; margin-top: -50px;}</style>';
+    echo '<style type="text/css">body.login #login h1 a { background: url(' . get_bloginfo('template_directory') . '/img/wdt_logo.png) no-repeat center top; height:146px; width:326px; margin-top: -50px;}</style>';
 }
 
 /**
  * Asignando dirección y Título al link del login
  */
-function ss_url_login(){
-	return '{%= author_url %}'; // The root of your site or any relative link
+function ss_url_login() {
+    return '{%= author_url %}'; // The root of your site or any relative link
 }
-function ss_url_title(){
-	return '{%= author_name %}'; // The title of your link
+
+function ss_url_title() {
+    return '{%= author_name %}'; // The title of your link
 }
 
 function remove_recent_comments_style() {
-	global $wp_widget_factory;
-	remove_action('wp_head', array($wp_widget_factory->widgets['WP_Widget_Recent_Comments'], 'recent_comments_style'));
+    global $wp_widget_factory;
+    remove_action('wp_head', array($wp_widget_factory->widgets['WP_Widget_Recent_Comments'], 'recent_comments_style'));
 }
 
 /*
@@ -136,89 +148,93 @@ function add_cap_editor() {
     // Editar opciones del tema
     $perfil->add_cap('edit_theme_options');
 }
+
 add_action('admin_init', 'add_cap_editor');
 
 add_action('admin_init', 'user_profile_fields_disable');
+
 function user_profile_fields_disable() {
     global $pagenow;
-    if ($pagenow ==='users.php' || $pagenow === 'user-new.php') {
-        add_action( 'admin_footer', 'user_list_fields_disable_js' );
+    if ($pagenow === 'users.php' || $pagenow === 'user-new.php') {
+        add_action('admin_footer', 'user_list_fields_disable_js');
         return;
     }
-    if ($pagenow!=='profile.php' && $pagenow!=='user-edit.php') {
+    if ($pagenow !== 'profile.php' && $pagenow !== 'user-edit.php') {
         return;
     }
     if (current_user_can('administrator')) {
         return;
     }
-    add_action( 'admin_footer', 'user_profile_fields_disable_js' );
+    add_action('admin_footer', 'user_profile_fields_disable_js');
 }
-
 
 /**
  * Disables selected fields in WP Admin user profile (profile.php, user-edit.php)
  */
 function user_profile_fields_disable_js() {
-?>
+    ?>
     <script>
-        jQuery(document).ready( function($) {
+        jQuery(document).ready(function ($) {
             var fields_to_disable = ['role'];
-            for(i=0; i<fields_to_disable.length; i++) {
-                if ( $('#'+ fields_to_disable[i]).length ) {
-                    $('#'+ fields_to_disable[i]).attr("disabled", "disabled");
+            for (i = 0; i < fields_to_disable.length; i++) {
+                if ($('#' + fields_to_disable[i]).length) {
+                    $('#' + fields_to_disable[i]).attr("disabled", "disabled");
                 }
             }
         });
     </script>
-<?php
+    <?php
+
 }
 
 /**
  * Remove option administrator in WP Admin user profile (users.php, user-new.php)
  */
 function user_list_fields_disable_js() {
-?>
+    ?>
     <script>
-        jQuery(document).ready( function($) {
+        jQuery(document).ready(function ($) {
             var fields_to_disable = ['new_role', 'role'];
-            for(i=0; i<fields_to_disable.length; i++) {
-                if ( $('#'+ fields_to_disable[i]).length ) {
-                    $('#'+ fields_to_disable[i]).find("option[value='administrator']").remove();
+            for (i = 0; i < fields_to_disable.length; i++) {
+                if ($('#' + fields_to_disable[i]).length) {
+                    $('#' + fields_to_disable[i]).find("option[value='administrator']").remove();
                 }
             }
         });
     </script>
-<?php
+    <?php
+
 }
 
 /**
  * Funciones para poder indexar en el buscador los custom fields
  * https://adambalee.com/search-wordpress-by-custom-fields-without-a-plugin/
  */
-function cf_search_join( $join ) {
+function cf_search_join($join) {
     global $wpdb;
 
-    if ( is_search() ) {
-        $join .=' LEFT JOIN '.$wpdb->postmeta. ' ON '. $wpdb->posts . '.ID = ' . $wpdb->postmeta . '.post_id ';
+    if (is_search()) {
+        $join .=' LEFT JOIN ' . $wpdb->postmeta . ' ON ' . $wpdb->posts . '.ID = ' . $wpdb->postmeta . '.post_id ';
     }
 
     return $join;
 }
-function cf_search_where( $where ) {
+
+function cf_search_where($where) {
     global $pagenow, $wpdb;
 
-    if ( is_search() ) {
+    if (is_search()) {
         $where = preg_replace(
-            "/\(\s*".$wpdb->posts.".post_title\s+LIKE\s*(\'[^\']+\')\s*\)/",
-            "(".$wpdb->posts.".post_title LIKE $1) OR (".$wpdb->postmeta.".meta_value LIKE $1)", $where );
+                "/\(\s*" . $wpdb->posts . ".post_title\s+LIKE\s*(\'[^\']+\')\s*\)/", "(" . $wpdb->posts . ".post_title LIKE $1) OR (" . $wpdb->postmeta . ".meta_value LIKE $1)", $where);
     }
 
     return $where;
 }
-function cf_search_distinct( $where ) {
+
+function cf_search_distinct($where) {
     global $wpdb;
 
-    if ( is_search() ) {
+    if (is_search()) {
         return "DISTINCT";
     }
 
