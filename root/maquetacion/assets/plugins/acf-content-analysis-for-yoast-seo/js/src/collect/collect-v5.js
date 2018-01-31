3d4a0446f4dc432acbb1ddcecbd1e2ebc8538f68
Saúl Solórzano <hola@saulsolorzano.com>
2018-01-31 11:02:33 -0300

    Quitando plugins que no son necesarios para la maquetacion

--- root/maquetacion/assets/plugins/acf-content-analysis-for-yoast-seo/js/src/collect/collect-v5.js
+++
@@ -1,9 +0,0 @@
-module.exports = function(){
-    return _.map(acf.get_fields(), function(field){
-
-        var field_data = jQuery.extend( true, {}, acf.get_data(jQuery(field)) );
-        field_data.$el = jQuery(field);
-        return field_data;
-
-    });
-};
--- root/maquetacion/assets/plugins/acf-content-analysis-for-yoast-seo/tests/js/system/nightwatch.conf.example.js
+++
@@ -1,4 +0,0 @@
-module.exports = (function(settings) {
-    settings.test_settings.default.launch_url = "http://yoast-acf-analysis.vvv.dev";
-    return settings;
-})(require('./nightwatch.json'));
